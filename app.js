1e899173be8def89d2705e2f6925e70a2ea0d038
Jsdwyer26 <Jsdwyer26@gmail.com>
2016-01-22 09:54:26 -0800

    setup ctrl for favorites and search with views, and navbar

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-var app = angular.module('instagramSearchApp', []);
+var app = angular.module('instagramSearchApp', ['ngRoute'] );
+
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'templates/search.html',
+      controller: 'SearchCtrl'
+    })
+    .when('/favorites', {
+      templateUrl: 'templates/favorites.html',
+      controller: 'FavoritesCtrl'
+    });
+
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+}]);
+
+app.controller('SearchCtrl', ['$scope', '$http', function($scope){
+  $scope.test = "This is the Search Ctrl";
+}]);
+
+app.controller('FavoritesCtrl', ['$scope', '$http', function($scope){
+  $scope.test = "This is the Favorites Ctrl";
+}]);
